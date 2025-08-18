4205e5d0d017e3d8920e711a615e2898e3c99044
kmgal3 <km.galazka3@student.uw.edu.pl>
2025-08-18 09:09:41 +0200

    Delete DFT_BandDrawer.py

--- DFT_BandDrawer.py
+++
@@ -1,8 +0,0 @@
-from DFT_Output import DFT_Output
-
-##### Autor: Karol Gałązka
-## Cel: przykładowe zastosowanie klasy DFT_Output
-
-Experiment = DFT_Output('VASP')
-Experiment.read_EIGENVAL()
-Experiment.band_Plot()
