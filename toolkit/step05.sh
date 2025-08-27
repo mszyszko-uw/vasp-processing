8f587741f9b62927c7e864d22d79ca882e6797da
kmgal3 <km.galazka3@student.uw.edu.pl>
2025-08-27 10:59:45 +0200

    Update step05.sh

--- toolkit/step05.sh
+++ toolkit/step05.sh
@@ -24,4 +24,4 @@ output=$(python3 toolkit.py --step t_dos --part dos)
 cd "$output"
 mpiexec vasp_std > log
 
-python3 $START_DIR/Postprocessing/DOS_post.py
+python3 $START_DIR/Postprocessing/DOS_report_post.py
