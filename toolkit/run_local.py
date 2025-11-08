6073d9e98479cfe7178476e6c6520f468ef638ff
maciejmarchwiany <maciej.marchwiany@gmail.com>
2025-11-08 20:17:33 +0100

    navigating through directories fixing

--- toolkit/run_local.py
+++ toolkit/run_local.py
@@ -1,18 +1,19 @@
 import subprocess
+import os
 
 def run_local(steps_def, step, vasp_command="mpiexec vasp_std", folder='./'):
-	command_to_run = ""
+	starting_path = os.getcwd()
+	command_to_run = "cd "+ folder
 
 	slurm_commands = steps_def[step]['cmd']
 	for line in slurm_commands .splitlines():	
 		if line.strip().startswith("module"):
 			command_to_run += ";"+line
 
-	command_to_run += '; cd '+ folder
 	command_to_run += "; python3 Preprocessing/toolkit.py --step t_" + step + " --part dry"
 	
 	command_to_run += ";" + vasp_command 
-	command_to_run += ";" + "cd .." 
+	command_to_run += ";" + "cd " + starting_path 
 	try:	
 		subprocess.run(command_to_run)
 	except:
