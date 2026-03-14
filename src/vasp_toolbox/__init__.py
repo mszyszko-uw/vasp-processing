61d223e0e45620400d6754fe71a68e3be6b7e721
maciejmarchwiany <maciej.marchwiany@gmail.com>
2026-03-14 21:32:46 +0100

    Delete src/vasp_toolbox directory

--- src/vasp_toolbox/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-vasp-processing: Toolkit for managing and postprocessing VASP DFT calculations
-on HPC/SLURM clusters.
-"""
-
-__version__ = "0.1.0"
--- src/vasp_toolbox/postprocessing/extract_magmom.py
+++
@@ -1,4 +0,0 @@
-from vaspout_h5 import vaspout_h5
-
-# The simplest way to extract MAGMOM from a calculation. Simply run this in any directory with a vaspout.h5 file.
-vaspout_h5().extract_magmom()
